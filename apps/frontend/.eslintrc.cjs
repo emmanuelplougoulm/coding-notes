95a6c48433ce7e44d4955e1c7caaec89b0311f5e
mf_DOM <emmanuel.plougoulm@gmail.com>
2025-05-28 15:34:08 +0200

    refactor: migrate from eslint and prettier to biome

--- apps/frontend/.eslintrc.cjs
+++ apps/frontend/.eslintrc.cjs
@@ -3,13 +3,5 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  extends: [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest'
-  }
-} 
+  extends: ['@coding-notes/eslint-config']
+}
